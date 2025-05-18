5cba79d9144c4ade93ee92ec2acbbc490eccf29f
Лысенко Евгений <evgeen@yandex.ru>
2025-05-18 10:02:54 +0300

    copy filter/tar -> filter/unzstd

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include <zstd.h>
+#include "read_input.h"
 
 #define SZ 1
 
@@ -10,8 +11,8 @@ char *read_input();
 
 int main() {
   char *src = read_input();
-  printf("Input length: %lu bytes\n", strlen(src));
-#if 0
+//   printf("Input length: %lu bytes\n", strlen(src));
+#if 1
   const int compressLevel = 3;
   size_t compressBound = ZSTD_compressBound(SZ);
   char *dst = malloc(compressBound);
@@ -30,36 +31,4 @@ int main() {
 
   free(src);
   return EXIT_SUCCESS;
-}
-
-// Read arbitrary lenght input from stdin.
-// Returns C-style string.
-// Returns null if memory allocation error happens.
-// Result supposed to be freed by client.
-char *read_input() {
-  size_t sz = 1;
-  char *ret = (char *)malloc(sz);
-  if (ret == NULL)
-    return ret;
-  size_t i = 0;
-  int ch;
-  while ((ch = getchar()) != EOF) {
-    if (i == sz - 1) {
-      sz *= 2;
-      char *tmp = (char *)realloc(ret, sz);
-      if (tmp == NULL) {
-        free(ret);
-        return NULL;
-      }
-      ret = tmp;
-    }
-    *(ret + i) = (char)ch;
-    ++i;
-  }
-  if (i + 1 < sz) {
-    char *tmp = (char *) realloc(ret, i + 1);
-    ret = tmp;
-  }
-  *(ret + i) = '\0';
-  return ret;
 }
---
+++ read_input.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// Read arbitrary lenght input from stdin.
+// Returns C-style string.
+// Returns null if memory allocation error happens.
+// Result supposed to be freed by client.
+char *read_input() {
+  size_t sz = 1;
+  char *ret = (char *)malloc(sz);
+  if (ret == NULL)
+    return ret;
+  size_t i = 0;
+  int ch;
+  while ((ch = getchar()) != EOF) {
+    if (i == sz - 1) {
+      sz *= 2;
+      char *tmp = (char *)realloc(ret, sz);
+      if (tmp == NULL) {
+        free(ret);
+        return NULL;
+      }
+      ret = tmp;
+    }
+    *(ret + i) = (char)ch;
+    ++i;
+  }
+  if (i + 1 < sz) {
+    char *tmp = (char *) realloc(ret, i + 1);
+    ret = tmp;
+  }
+  *(ret + i) = '\0';
+  return ret;
+}
